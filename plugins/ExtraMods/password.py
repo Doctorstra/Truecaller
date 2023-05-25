60129e775e9714ba5a904df7d9409bbd2288217a
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2023-05-25 12:45:16 +0530

    Update password.py

--- plugins/ExtraMods/password.py
+++ plugins/ExtraMods/password.py
@@ -1,1 +1,20 @@
+import random, os
+from pyrogram import Client, filters, enums 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(filters.command(["password", 'password']))
+async def password(bot, update):
+    message = await update.reply_text(text="`Processing...`")
+    password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
+    if len(update.command) > 1:
+        qw = update.text.split(" ", 1)[1]
+    else:
+        ST = ["5", "7", "6", "9", "10", "12", "14", "8", "13"] 
+        qw = random.choice(ST)
+    limit = int(qw)
+    random_value = "".join(random.sample(password, limit))
+    txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('꧁♕𝐃𝐀𝐃𝐒 𝐋𝐈𝐍𝐊𝐒꧂', url='https://t.me/dads_Links')]])
+    await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
 
